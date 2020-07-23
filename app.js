69fb8f613e7d2d9477f2cbba0cfed3b15f13e1d6
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-23 14:44:50 -0700

    movie and and new Movie

--- app.js
+++ app.js
@@ -18,4 +18,34 @@ console.log(sloth);
 console.log(sloth2);
 console.log(sloth2.terrain);
 
+function User(name) {
+    this.name = name;
+}
 
+let Adam = new User("adam");
+console.log(Adam.name);
+
+function Movie(title, director, color, vibe) {
+    this.title = title;
+    this.director = director;
+    this.color = color;
+    this.vibe = vibe;
+}
+
+let royalTenebaums = new Movie("the royal tenenbaums",
+    "wes anderson",
+    "pale primary",
+    "death but make it funny");
+console.log(royalTenebaums);
+
+let palmSprings = new Movie("palm springs",
+    "not andy samberg",
+    "bright",
+    "groundhogs day but it's not winter");
+console.log(palmSprings);
+
+let twinPeaks = new Movie("twin peaks",
+    "david lynch",
+    "red and black",
+    "real spoopy");
+console.log(twinPeaks);
