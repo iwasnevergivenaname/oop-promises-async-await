d37748eecd8f76653e1c791408a5108045926906
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-23 15:34:27 -0700

    add class for github profile

--- app.js
+++ app.js
@@ -1,51 +1,80 @@
-const sloth = {
-    toes: 2,
-    moss: true,
-    fast: false,
-    cute: true,
-    terrain: "rain forest",
-    name: "belle"
-}
+// const sloth = {
+//     toes: 2,
+//     moss: true,
+//     fast: false,
+//     cute: true,
+//     terrain: "rain forest",
+//     name: "belle"
+// }
+//
+// const sloth2 = {
+//     name: "jazz",
+//     color: "brown"
+// }
+//
+// sloth2.__proto__= sloth;
+//
+// console.log(sloth);
+// console.log(sloth2);
+// console.log(sloth2.terrain);
+//
+// function User(name) {
+//     this.name = name;
+// }
+//
+// let Adam = new User("adam");
+// console.log(Adam.name);
 
-const sloth2 = {
-    name: "jazz",
-    color: "brown"
-}
-
-sloth2.__proto__= sloth;
-
-console.log(sloth);
-console.log(sloth2);
-console.log(sloth2.terrain);
-
-function User(name) {
-    this.name = name;
-}
-
-let Adam = new User("adam");
-console.log(Adam.name);
-
-function Movie(title, director, color, vibe) {
-    this.title = title;
-    this.director = director;
-    this.color = color;
-    this.vibe = vibe;
+class Movie {
+    constructor(title, director, color, vibe) {
+        this.title = title;
+        this.director = director;
+        this.color = color;
+        this.vibe = vibe;
+        }
+    intro() {
+        console.log("hi i'm " + this.director + " and i directed " + this.title +
+            " which uses a lot of " + this.color + " colors to make something that is like " +
+            this.vibe);
+    }
 }
 
 let royalTenebaums = new Movie("the royal tenenbaums",
     "wes anderson",
     "pale primary",
     "death but make it funny");
-console.log(royalTenebaums);
+royalTenebaums.intro();
 
 let palmSprings = new Movie("palm springs",
     "not andy samberg",
     "bright",
     "groundhogs day but it's not winter");
-console.log(palmSprings);
+palmSprings.intro();
 
 let twinPeaks = new Movie("twin peaks",
     "david lynch",
     "red and black",
     "real spoopy");
-console.log(twinPeaks);
+twinPeaks.intro();
+
+class GitHubProfile {
+    constructor(username, name, url) {
+        this.username = username;
+        this.name = name;
+        this.url = url;
+    }
+    intro() {
+        console.log(`my name is ${this.name} and i chose 
+        ${this.username} because i like the office and you can 
+        find my repos at ${this.url}`);
+    }
+}
+
+fetch("https://api.github.com/users/iwasnevergivenaname")
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    console.log(data);
+});
+
